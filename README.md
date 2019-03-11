d58c044ea3254a453089745ae48b75e03d1a6ef4
pingo <--get>
2019-03-11 16:02:10 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+State Management System with vanilla JavaScript
+================================================
+
+  The PubSub pattern loops through all of the subscriptions and fires their callbacks with that payload. It’s a great way of creating a pretty elegant reactive flow for your app and we can do it with only a few lines of code.
+ 
+  
---
+++ src/js/lib/component.js
@@ -0,0 +1,18 @@
+import '../store/store';
+import Store from '../store/store';
+
+export default class Component {
+  constructor(props ={}){
+    let self = this;
+
+    this.render = this.render || function(){};
+
+    if(props.store instanceof Store){
+      props.store.events.subscribe('stateChange', () => self.render())
+    }
+
+    if(prompt.hasOwnProperty('element')){
+      this.element = props.element;
+    }
+  }
+}
